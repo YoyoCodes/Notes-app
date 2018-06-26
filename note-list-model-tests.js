f4f11bfdbc24a085164873540cbda3399fe0ada1
Laura Petre <laura.d.petre@gmail.com>
2018-06-26 11:59:26 +0100

    added second test for listing notes - refractored

--- note-list-model-tests.js
+++ note-list-model-tests.js
@@ -2,13 +2,10 @@
   function testListNotesStoredInArray(){
     var note = new Note();
 
-    if(note.notes !== []) {
-      throw new Error("The Note object is not initializing with an empty array")
-    }
-
     note.add("new note");
-    if(!note.seeStoredNotes.includes("new note")) {
-      throw new Error("The added note was not found in the array")
+    if(!note.seeStoredNotes().includes("new note")) {
+      throw new Error("The added note was not found in the array");
     }
-  }
+  };
+testListNotesStoredInArray();
 })(this);
