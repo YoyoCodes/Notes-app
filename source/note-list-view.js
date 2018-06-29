3d59cf81d7ca1d9fb6c69c64bad3df27ff821f5f
Laura Petre <laura.d.petre@gmail.com>
2018-06-29 16:50:53 +0100

    Implemented display the whole note when you click on the link

--- source/note-list-view.js
+++ source/note-list-view.js
@@ -7,7 +7,7 @@
     var htmlstring = "<ul>"
     for (var i= 0; i < this.notesList.seeStoredNotes().length; i++){
       htmlstring = htmlstring +
-                    "<li><a href='#notes//" +
+                    "<li><a href='#notes" +
                     i+
                     "'><div id='"+
                     i +
