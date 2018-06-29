ea8cce00fbd3ef999a1c4c70483908e1dc861356
Laura Petre <laura.d.petre@gmail.com>
2018-06-29 15:17:01 +0100

    Uptated controller to create new instance of Single View

--- source/note-controller.js
+++ source/note-controller.js
@@ -1,11 +1,18 @@
 (function(exports) {
   function NoteController(noteModelList) {
+    this.noteModelList = noteModelList;
     this.noteListView = new NoteListView(noteModelList);
   };
 
   NoteController.prototype.insert = function() {
     var element = document.getElementById("app");
     element.innerHTML = this.noteListView.getHTML();
+  }
+
+  NoteController.prototype.getView = function(id){
+    var noteModel = this.noteModelList.findNoteById(id);
+    var singleView = new SingleView(noteModel);
+    return singleView.getHTML();
   }
 
   exports.NoteController = NoteController;
